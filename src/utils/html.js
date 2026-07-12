96cab1d6a1ad821555ef43b7117cdba62cec1f3f
VS Code Clone <dev@vscode-clone.dev>
2026-07-12 10:00:57 +0330

    feat: add HTML utility (stripHtml, escapeAttr, createDocumentFragment)
    Co-authored-by: erfan753

---
+++ src/utils/html.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview
+ * HTML utility helpers.
+ */
+
+export function stripHtml(html) {
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  return div.textContent || div.innerText || '';
+}
+
+export function escapeAttr(str) {
+  return String(str).replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
+}
+
+export function createDocumentFragment(strings, ...values) {
+  const html = strings.reduce((acc, str, i) => acc + str + (values[i] || ''), '');
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content;
+}

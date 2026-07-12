30eb9a28aff7bf539c12ec4a0a54eb4e637e6dc5
VS Code Clone <dev@vscode-clone.dev>
2026-07-12 09:57:23 +0330

    feat: add CSS utility (addClass, toggleClass, setStyle, etc.)
    Co-authored-by: erfan753

---
+++ src/utils/css.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview
+ * CSS utility helpers.
+ */
+
+export function addClass(el, cls) {
+  el.classList.add(cls);
+}
+
+export function removeClass(el, cls) {
+  el.classList.remove(cls);
+}
+
+export function toggleClass(el, cls, force) {
+  el.classList.toggle(cls, force);
+}
+
+export function hasClass(el, cls) {
+  return el.classList.contains(cls);
+}
+
+export function setStyle(el, prop, value) {
+  el.style[prop] = value;
+}
+
+export function getStyle(el, prop) {
+  return getComputedStyle(el)[prop];
+}
